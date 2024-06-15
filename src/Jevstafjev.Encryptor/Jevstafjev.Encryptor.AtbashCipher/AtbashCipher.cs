15b125e83462952fb3a1c158edeebff4c9aa5814
Apelsinka18 <roman.jevstafjev@tptlive.ee>
2024-06-15 14:23:47 +0300

    Add Atbash cipher

---
+++ src/Jevstafjev.Encryptor/Jevstafjev.Encryptor.AtbashCipher/AtbashCipher.cs
@@ -0,0 +1,66 @@
+﻿using Jevstafjev.Encryptor.Contracts;
+using System.Text;
+
+namespace Jevstafjev.Encryptor.AtbashCipher
+{
+    public class AtbashCipher : ICipher
+    {
+        public string? Encrypt(string value)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var item in value)
+            {
+                if (item >= 'A' && item <= 'Z')
+                {
+                    var opposite = (char)('A' + ('Z' - item));
+                    stringBuilder.Append(opposite);
+
+                    continue;
+                }
+
+                if (item >= 'a' && item <= 'z')
+                {
+                    var opposite = (char)('a' + ('z' - item));
+                    stringBuilder.Append(opposite);
+
+                    continue;
+                }
+
+                stringBuilder.Append(item);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public string? Decrypt(string value)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var item in value)
+            {
+                if (item >= 'A' && item <= 'Z')
+                {
+                    var opposite = (char)('A' + ('Z' - item));
+                    stringBuilder.Append(opposite);
+
+                    continue;
+                }
+
+                if (item >= 'a' && item <= 'z')
+                {
+                    var opposite = (char)('a' + ('z' - item));
+                    stringBuilder.Append(opposite);
+
+                    continue;
+                }
+
+                stringBuilder.Append(item);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public string Name => "Atbash cipher";
+    }
+}
