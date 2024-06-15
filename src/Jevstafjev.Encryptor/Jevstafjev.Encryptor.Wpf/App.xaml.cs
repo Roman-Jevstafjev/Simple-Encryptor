4c297baeb4f1db65120e9154c583f6e7808f1efe
Apelsinka18 <roman.jevstafjev@tptlive.ee>
2024-06-15 13:08:10 +0300

    Add the ciphers to the dependency container

--- src/Jevstafjev.Encryptor/Jevstafjev.Encryptor.Wpf/App.xaml.cs
+++ src/Jevstafjev.Encryptor/Jevstafjev.Encryptor.Wpf/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using DryIoc;
+using Jevstafjev.Encryptor.Contracts;
 using Jevstafjev.Encryptor.Wpf.Views;
 using Prism.Ioc;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace Jevstafjev.Encryptor.Wpf
@@ -9,6 +12,22 @@ namespace Jevstafjev.Encryptor.Wpf
     {
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            var pluginsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Plugins");
+            if (!Directory.Exists(pluginsDirectory))
+            {
+                return;
+            }
+
+            var files = Directory.GetFiles(pluginsDirectory, "*.dll");
+            foreach (var file in files)
+            {
+                var assembly = Assembly.LoadFile(file);
+                var cipherTypes = assembly.GetExportedTypes().Where(x => x.IsAssignableTo(typeof(ICipher))).ToList();
+                foreach (var type in cipherTypes)
+                {
+                    containerRegistry.Register(typeof(ICipher), type);
+                }
+            }
         }
 
         protected override Window CreateShell()
