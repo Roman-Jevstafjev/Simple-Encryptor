f542974ea6f67ea98e1e2053f7b85c2a490cc4c4
Apelsinka18 <roman.jevstafjev@tptlive.ee>
2024-06-15 11:48:42 +0300

    Create Contracts project

---
+++ src/Jevstafjev.Encryptor/Jevstafjev.Encryptor.Contracts/ICipher.cs
@@ -0,0 +1,11 @@
+﻿namespace Jevstafjev.Encryptor.Contracts
+{
+    public interface ICipher
+    {
+        string? Encrypt(string value);
+
+        string? Decrypt(string value);
+
+        string Name { get; }
+    }
+}
